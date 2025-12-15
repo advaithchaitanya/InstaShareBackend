01a362ccafe085717918923d86921be527a163d4
Advaith Chaitanya <advaithchaitanya@gmail.com>
2025-12-15 16:01:51 +0530

    Remove Cloudinary credentials from CloudinaryConfig
    Removed sensitive Cloudinary credentials from configuration.

--- insta-share-app/src/main/java/com/example/insta_share_app/config/CloudinaryConfig.java
+++ insta-share-app/src/main/java/com/example/insta_share_app/config/CloudinaryConfig.java
@@ -12,9 +12,9 @@ public class CloudinaryConfig {
     @Bean
     public Cloudinary getCloudinary(){
         Map map=new HashMap();
-        map.put("cloud_name","dzfwhbukv");
-        map.put("api_key","373465495131555");
-        map.put("api_secret","JlA9EajV5c0GrNazGV2tG8ugtaI");
+        map.put("cloud_name","");
+        map.put("api_key","");
+        map.put("api_secret","");
         map.put("secure",true);
         return new Cloudinary(map);
     }
